b995a43f2db28cd85ef10a29adad994db74d93d8
zhaozimei <1140173180@qq.com>
2016-05-29 21:18:16 +0800

    Update calculate-median.js

--- src/calculate-median.js
+++ src/calculate-median.js
@@ -1,5 +1,22 @@
 function calculate_median(arr) {
-  //请勿改动函数名
-}
+  function sortNumber(a,b){
+		return a - b;
+	}
+	var brr = [];
+	var num = 0;
+	for(var i=0; i<arr.length; i++){
+		if((i%2)!=0){
+			brr.push(arr[i])
+		}
+	}
+	var a = Math.floor(brr.length/2)
+	brr.sort(sortNumber)
+	
+	if(brr.length%2 == 0){
+	     num = (brr[a]+brr[a-1])/2
+	}else{
+		 num = brr[a]
+	}
+	return num;
 
 module.exports = calculate_median;
